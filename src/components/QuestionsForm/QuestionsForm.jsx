c3f9b4848aef0882cbdd25c62b356e6b2a63b043
AlMamunTalukder <almamuntalukder0@gmail.com>
2024-01-09 23:25:47 +0600

    question form add

---
+++ src/components/QuestionsForm/QuestionsForm.jsx
@@ -0,0 +1,32 @@
+import CropOriginalIcon from "@mui/icons-material/CropOriginal";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import CheckBoxIcon from "@material-ui/icons/CheckBox";
+import ShortTextIcon from "@material-ui/icons/ShortText";
+import SubjectIcon from "@material-ui/icons/Subject";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import CloseIcon from "@material-ui/icons/Close";
+
+import FilterNoneIcon from "@material-ui/icons/FilterNone";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import Select from "@material-ui/core/Select";
+import Switch from "@material-ui/core/Switch";
+
+import { BsTrash } from "react-icons/bs";
+import { IconButton } from "@material-ui/core";
+
+import { BsFileText } from "react-icons/bs";
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Button from "@material-ui/core/Button";
+import { FcRightUp } from "react-icons/fc";
+
+import Radio from "@material-ui/core/Radio";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+const QuestionsForm = () => {
+  return <div></div>;
+};
+
+export default QuestionsForm;
